36b9af6f1ae4388bc663d5782c19a8e9bf8705cd
Richalene <dalisaymary18@gmail.com>
2026-08-27 11:35:03 +0800

    fixed images

--- dalisay-portfolio/src/App.jsx
+++ dalisay-portfolio/src/App.jsx
@@ -12,12 +12,12 @@ function App() {
     <main
       className="relative min-h-screen overflow-y-auto bg-scroll bg-cover bg-center"
       style={{
-        backgroundImage: "url('/src/assets/bgpurple.png')",
+        backgroundImage: "url('https://media.discordapp.net/attachments/1101141739596697601/1542376121176432710/1000023754.jpg?ex=6a910133&is=6a8fafb3&hm=5dbbf9c2fee6da93defb5ed3ed248660c2c317d48bbe7db6514f54ac26de2722&=&format=webp&width=486&height=1024')",
         backgroundSize: '40%',
       }}
     >
-      <img src="/src/assets/sidebarleft.png" alt="" className="fixed top-0 left-0 h-screen z-50" />
-      <img src="/src/assets/sidebarright.png" alt="" className="fixed top-0 right-0 h-screen z-50" />
+      <img src="https://media.discordapp.net/attachments/1101141739596697601/1542376070907433051/sidebarright.png?ex=6a910127&is=6a8fafa7&hm=168489a757ee5a6d4cd3ae8fe3b82127c3a84a11fd468bee7e5ee510e8d8ea14&=&format=webp&quality=lossless&width=230&height=1024" alt="" className="fixed top-0 left-0 h-screen z-50" />
+      <img src="https://media.discordapp.net/attachments/1101141739596697601/1542376017761669211/sidebarright1.png?ex=6a91011a&is=6a8faf9a&hm=c6101b02cfe482b6a7c78529d3a0616465ad261cb7ae15c32b9954c9f129f77f&=&format=webp&quality=lossless&width=230&height=1024" alt="" className="fixed top-0 right-0 h-screen z-50" />
 
       <Nav />
 
