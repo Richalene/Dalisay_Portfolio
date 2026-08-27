178f6d4dbb3ef6d9fee3e1af0099d699f9f3f647
Richalene <dalisaymary18@gmail.com>
2026-08-27 17:48:21 +0800

    edited footer.jsx to add a link to Nataniel Nemes for legal support

--- dalisay-portfolio/src/components/Footer.jsx
+++ dalisay-portfolio/src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         </p>
 
         <div className="flex items-center gap-2 text-xs font-mono text-purple-400/50">
-          made with love, red bull, and <a href="https://media.discordapp.net/attachments/1444774902702805067/1542467828743933952/3647743584.png?ex=6a91569c&is=6a90051c&hm=f81017012b2c812c68ebf0bb0d5102906fb4806a23373e27ff202995bc311eae&=&format=webp&quality=lossless&width=640&height=360">Nataniel Nemes</a> for moral support :3
+          made with love, red bull, and <a href="https://media.discordapp.net/attachments/1444774902702805067/1542467828743933952/3647743584.png?ex=6a91569c&is=6a90051c&hm=f81017012b2c812c68ebf0bb0d5102906fb4806a23373e27ff202995bc311eae&=&format=webp&quality=lossless&width=640&height=360" target="_blank" rel="noopener noreferrer">Nataniel Nemes</a> for legal support :3
         </div>
       </div>
     </footer>
